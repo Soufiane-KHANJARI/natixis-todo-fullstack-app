bdd774b589d08cee1ea2bcf36a8d58fad679c16c
sofiene55 <khanjarisofiene@gmail.com>
2025-10-27 10:43:07 +0100

    configuration des routes

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,36 +1,32 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 
-export const routes: Routes =  [
-  { path: '', loadChildren: () => import('./task/task-module').then(m => m.TaskModule) },
+export const routes: Routes = [
+  // Redirection par défaut
   {
     path: '',
     redirectTo: '/tasks',
     pathMatch: 'full'
   },
+  
+  // Liste des tâches
   {
     path: 'tasks',
-    children: [
-      {
-        path: '',
-        loadComponent: () => import('./task/components/task-list/task-list.component')
-          .then(m => m.TaskListComponent)
-      },
-      {
-        path: ':id',
-        loadComponent: () => import('./task/components/task-detail/task-detail.component')
-          .then(m => m.TaskDetailComponent)
-      }
-    ]
+    loadComponent: () => import('./task/components/task-list/task-list.component')
+      .then(m => m.TaskListComponent),
+    title: 'Mes Tâches'
   },
+  
+  // Détail d'une tâche (avec paramètre :id)
+  {
+    path: 'tasks/:id',
+    loadComponent: () => import('./task/components/task-detail/task-detail.component')
+      .then(m => m.TaskDetailComponent),
+    title: 'Détail de la tâche'
+  },
+  
+  // Page 404 - redirection vers la liste
   {
     path: '**',
     redirectTo: '/tasks'
   }
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
